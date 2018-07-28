1d5cc2cecb2ade4b58d419c98e722a4e6b0bf158
Rica May Rico <emeraldrico15@gmail.com>
2018-07-28 17:49:56 +0800

    Task Performance 1

---
+++ app/src/main/java/com/rico/task_performance_1/DisplayMessageActivity.java
@@ -0,0 +1,20 @@
+package com.rico.task_performance_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+}
